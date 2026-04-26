f342aaa4f4e506401d5b5e72bc88262c3b2bdb35
NGUYEN-THI-HUYNH-NHU <huynhnhu.connect@gmail.com>
2026-04-26 15:14:31 +0700

    [add]: auth & db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { toast } from "sonner";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+const Home = () => {
   return (
-    <Button size="xs" onClick={() => toast.success("Hello world")}>
-      Click me
-    </Button>
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-background">
+      <h1 className="text-2xl font-semibold">Wellcome to Sonority</h1>
+      <div className="flex items-center gap-4">
+        <OrganizationSwitcher />
+        <UserButton />
+      </div>
+    </div>
   );
-}
+};
+
+export default Home;
