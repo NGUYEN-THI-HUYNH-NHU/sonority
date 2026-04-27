96f7fbe35c423b3c4a2d31568cb3f34ce95414cf
NGUYEN-THI-HUYNH-NHU <huynhnhu.connect@gmail.com>
2026-04-27 19:48:35 +0700

    [add]: backend infrastructure

---
+++ src/app/(dashboard)/test/health-check.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+export function HealthCheck() {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.health.queryOptions());
+  return (
+    <div>
+      <p>trpc status</p>
+      <p>{data.status}</p>
+    </div>
+  );
+}
